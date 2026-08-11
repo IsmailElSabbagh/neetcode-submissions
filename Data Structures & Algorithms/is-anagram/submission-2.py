ae0f69799feda5b2b908d1955eb0e34ecc7fbd59
IsmailElSabbagh <elsabbagh-giessen@outlook.de>
2026-08-11 18:26:56 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        list1=[]
+        list2=[]
+        for i in s:
+            list1.append(i)
+        for j in t:
+            list2.append(j)
+        list1.sort()
+        list2.sort()
+        return True if list1 == list2 else False
